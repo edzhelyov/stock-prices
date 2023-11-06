59d4648a234ae7709b36dfbde7a1baebdc7f7a57
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2023-11-06 13:25:31 +0200

    Add client-side validations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,20 +6,45 @@ interface Data {
   buyTime: number,
   buyPrice: number,
   sellTime: number,
-  sellPrice: number
+  sellPrice: number,
+}
+interface Error {
+  message: string[],
 }
 
 function App() {
   const submit = async () => {
-    const response = await fetch(`/api/max-profit?startTime=${startTime}&endTime=${endTime}`);
-    const data = await response.json();
-    setData(data);
-    if (data.shouldBuy) {
-      const shares = Math.floor(parseInt(funds) / data.buyPrice);
-      const profit = data.sellPrice * shares - parseInt(funds);
-      setProfit(profit);
-      setShares(shares);
+    setData(null);
+
+    if (!validate()) {
+      return;
     }
+
+    const response = await fetch(`/api/max-profit?startTime=${startTime}&endTime=${endTime}`);
+    const response_body = await response.json();
+
+    if (!response.ok) {
+      setError(response_body);
+    } else {
+      setData(response_body);
+      setError(null);
+
+      if (response_body.shouldBuy) {
+        const shares = Math.floor(parseInt(funds) / response_body.buyPrice);
+        const profit = response_body.sellPrice * shares - parseInt(funds);
+        setProfit(profit);
+        setShares(shares);
+      }
+    }
+  };
+
+  const validate = () => {
+    if (isNaN(parseInt(funds)) || parseInt(funds) <= 0) {
+      setError({message: ['funds should be a number greater than 0']})
+      return false;
+    }
+
+    return true;
   };
 
   const [funds, setFunds] = React.useState('');
@@ -28,27 +53,41 @@ function App() {
   const [data, setData] = React.useState<Data | null>(null);
   const [profit, setProfit] = React.useState(0);
   const [shares, setShares] = React.useState(0);
+  const [error, setError] = React.useState<Error | null>(null);
 
   return (
     <div className="App container">
       <h1 className="text-align-center">Basic profit calculator</h1>
-      <div className="row">
+      <div className="row mb-3">
         <p className="col-auto">
           Enter the time period and the available funds you want to invest and the calculator will give you the most profitable
           time to buy and sell.
         </p>
         <p>
           <small>NOTE: We support only UNIX timestamps for the time fields.</small>
-          <details>
-            <summary><small>Click for hints about the current data</small></summary>
-            <ul>
-              <li>Submit default values will return 12$ for buy and 97$ for sell.</li>
-              <li>Start time of 1672531210 will return 10$ for buy and 95$ for sell</li>
-              <li>Start time of 1672531258 will return no profitable situation.</li>
-            </ul>
-          </details>
         </p>
+        <details>
+          <summary><small>Click for hints about the current data</small></summary>
+          <ul>
+            <li>Submit default values will return 12$ for buy and 97$ for sell.</li>
+            <li>Start time of 1672531210 will return 10$ for buy and 95$ for sell</li>
+            <li>Start time of 1672531258 will return no profitable situation.</li>
+          </ul>
+        </details>
       </div>
+      {error && (
+        <div className="row">
+          <div className='col-auto'>
+            <ul className=" text-danger">
+            {error.message.map((message, index) => (
+              <li key={index}>
+                {message}
+              </li>
+            ))}
+            </ul>
+          </div>
+        </div>
+      )}
       <div className="row">
         <div className="col-auto">
           <label className="form-label" htmlFor="funds">Available funds ($):</label>
@@ -67,18 +106,20 @@ function App() {
           <button className="btn btn-primary position-absolute bottom-0" onClick={submit}>Submit</button>
         </div>
       </div>
-      <div className="row mt-3">
-        {data && data.shouldBuy ? (
-          <div className="col-auto">
-            <p>
-              Buy {shares} shares at {data.buyTime} for {data.buyPrice}$ and sell at {data.sellTime} for {data.sellPrice}$.
-              For a net profit of {profit}$.
-            </p>
-          </div>
-        ) : (
-          <div>There is no profitable opportunity in the specified time period.</div>
-        )}
-      </div>
+      {data && (
+        <div className="row mt-3">
+          {data.shouldBuy ? (
+            <div className="col-auto">
+              <p>
+                Buy {shares} shares at {data.buyTime} for {data.buyPrice}$ and sell at {data.sellTime} for {data.sellPrice}$.
+                For a net profit of {profit}$.
+              </p>
+            </div>
+          ) : (
+            <div>There is no profitable opportunity in the specified time period.</div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
